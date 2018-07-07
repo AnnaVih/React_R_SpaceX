8c0c072e760521164e03b97cf53cf44c88cd9a94
annaVih <anna-kobzii@mail.ru>
2018-07-07 12:27:09 +0100

    Config react-redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Layout from './hoc/Layout/Layout';
 import Home from './containers/Home';
 import About from './containers/About';
 
 const App = () => (
-  <Layout>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-    </Switch>
-  </Layout>
+  <BrowserRouter>
+    <Layout>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
+    </Layout>
+  </BrowserRouter>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './App';
 
-const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(app, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware()}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
