6b577e827c5a6c13de46e93267542ab5d36f14f2
annaVih <anna-kobzii@mail.ru>
2018-07-07 12:03:26 +0100

    Change path to the file

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './containers/App';
+import App from './App';
 
 const app = (
   <BrowserRouter>
