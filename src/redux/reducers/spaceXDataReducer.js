f5cf773ec2e95f437962e5cbba7c9b331a51bb39
annaVih <anna-kobzii@mail.ru>
2018-10-31 22:44:42 +0000

    Set loading state

--- src/redux/reducers/spaceXDataReducer.js
+++ src/redux/reducers/spaceXDataReducer.js
@@ -2,20 +2,28 @@ import * as actionTypes from '../actions/action_types'
 
 const initialState = {
   companyInfo: [],
-  error: false
+  error: false,
+  loading: false
 }
 const reducer = (state = initialState, action) => {
   switch (action && action.type) {
+    case actionTypes.FETCH_COMPANY_INFO:
+      return {
+        ...state,
+        loading: true
+      }
     case actionTypes.DISPLAY_COMPANY_INFO:
       return {
         ...state,
         companyInfo: action.payload,
-        error: false
+        error: false,
+        loading: false
       }
     case actionTypes.DISPLAY_COMPANY_INFO_FAILED:
       return {
         ...state,
-        error: true
+        error: true,
+        loading: false
       }
     default:
       return state
