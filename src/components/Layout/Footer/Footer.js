e9f04cf743f9ea8a2802ee0a9f45d9e515abbead
annaVih <anna-kobzii@mail.ru>
2019-01-01 22:00:41 +0000

    Include social links and improve copyright date section

--- src/components/Layout/Footer/Footer.js
+++ src/components/Layout/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
 
 const FooterWrapper = styled.div`
@@ -15,15 +16,36 @@ const FooterContent = styled.div`
   max-width: 1280px;
   min-width: 360px;
   color: white;
+  display: flex;
+  justify-content: space-between;
+`
+
+const ContentDivs = styled.div`
+  display: flex;
+  justify-content: space-between;
 `
 
 const Footer = () => (
   <FooterWrapper>
     <SubFooter footerPadding={40} footerBg="#181c1f">
-      <FooterContent>Footer Top</FooterContent>
+      <FooterContent>In development...</FooterContent>
     </SubFooter>
-    <SubFooter footerPadding={22} footerBg="#0e1112">
-      <FooterContent>Footer Bottom</FooterContent>
+    <SubFooter footerPadding={20} footerBg="#0e1112">
+      <FooterContent>
+        <ContentDivs width={25}>
+          <p>Follow SpaceX |</p>
+          <NavLink to="https://twitter.com/spacex">Twitter |</NavLink>
+          <NavLink to="https://www.youtube.com/spacex">YouTube |</NavLink>
+          <NavLink to="https://www.flickr.com/photos/spacex">Flickr |</NavLink>
+          <NavLink to="https://www.instagram.com/spacex">Instagram</NavLink>
+        </ContentDivs>
+        <ContentDivs width={75}>
+          <p>
+            Copyright &copy; {new Date().getFullYear()} Developed by Anna Vihrogonova. It is a result of hard work,
+            passion and learning from failure.
+          </p>
+        </ContentDivs>
+      </FooterContent>
     </SubFooter>
   </FooterWrapper>
 )
